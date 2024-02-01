856d6de931eca3a7d73c710bd39367255565684c
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2024-02-01 04:19:02 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { StyledHeader } from './Header.styled';
+import { StyledLink } from './Header.styled';
 
 const Header = () => {
   return (
     <StyledHeader>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/catalog">Catalog</Link>
-        <Link to="/favorites">Favorites</Link>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/catalog">Catalog</StyledLink>
+        <StyledLink to="/favorites">Favorites</StyledLink>
       </nav>
     </StyledHeader>
   );
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,6 +1,29 @@
 import styled from 'styled-components';
+// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const StyledHeader = styled.header`
   width: 100%;
   height: 50px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border-bottom: 2px solid var(--options-text-color);
+`;
+
+export const StyledLink = styled(NavLink)`
+  padding: 12px 50px;
+  display: inline-block;
+  border: 2px solid transparent;
+  box-sizing: border-box;
+  color: var(--button-color);
+  transition: transform var(--transition);
+
+  &:hover {
+    color: var(--accent-color);
+    transform: scale(1.1);
+
+    border-color: var(--accent-color);
+    border-radius: 12px;
+    padding: 12px 50px;
+  }
 `;
