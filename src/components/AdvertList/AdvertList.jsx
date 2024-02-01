43fbebf1695861775cb7df356c33e6a31089b7cc
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2024-02-01 22:06:13 +0100

    AdvertList

--- src/components/AdvertList/AdvertList.jsx
+++ src/components/AdvertList/AdvertList.jsx
@@ -1,27 +1,26 @@
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-// import { selectCars } from '../../../../services/state/cars/carsSelectors';
+import { selectCars } from '../../../../services/state/cars/carsSelectors';
 
-// import AdvertItem from '../AdvertItem';
-
-// import { StyledAdvertList } from './AdvertList.styled';
-
-// const AdvertList = () => {
-//   const cars = useSelector(selectCars);
-
-//   return (
-//     <StyledAdvertList>
-//       {cars.map(car => (
-//         <AdvertItem key={car.id} car={car} />
-//       ))}
-//     </StyledAdvertList>
-//   );
-// };
-
-// export default AdvertList;
+import AdvertItem from '../AdvertItem';
+import { StyledAdvertList } from './AdvertList.styled';
 
 const AdvertList = () => {
-  return <div>AdvertList</div>;
+  const cars = useSelector(selectCars);
+
+  return (
+    <StyledAdvertList>
+      {cars.map(car => (
+        <AdvertItem key={car.id} car={car} />
+      ))}
+    </StyledAdvertList>
+  );
 };
 
 export default AdvertList;
+
+// const AdvertList = () => {
+//   return <div>AdvertList</div>;
+// };
+
+// export default AdvertList;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,5 +1,33 @@
-const Catalog = () => {
-  return <div>Catalog</div>;
+// const Catalog = () => {
+//   return <div>Catalog</div>;
+// };
+
+// export default Catalog;
+
+import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+
+import { getAllCars } from '../../services/state/cars/carsOperations';
+// import { selectIsLoading } from '../../services/state/cars/carsSelectors';
+
+// import Loader from '../../components/ui/Loader';
+import AdvertList from '../components/AdvertList';
+
+const CatalogPage = () => {
+  const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(getAllCars());
+  }, [dispatch]);
+
+  return (
+    <section>
+      <AdvertList />
+      {/* {isLoading && <Loader />} */}
+    </section>
+  );
 };
 
-export default Catalog;
+export default CatalogPage;
