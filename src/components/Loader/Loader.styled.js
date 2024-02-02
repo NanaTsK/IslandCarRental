ee5b2ed781b96552033c879c0eb834d0286892c1
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2024-02-02 20:28:01 +0100

    loader

---
+++ src/components/Loader/Loader.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledLoaderWrapper = styled.div`
+  position: fixed;
+  z-index: 100;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
