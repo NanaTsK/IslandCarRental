28aac9969ee453e9860191037e4ae421a626c15c
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2024-02-03 21:52:10 +0100

    layout

--- src/components/MainContainer/MainContainer.styled.js
+++ src/components/MainContainer/MainContainer.styled.js
@@ -6,12 +6,12 @@ export const Container = styled.div`
   margin: 0 auto;
   padding: 0 20px;
 
-  @media screen and (min-width: 768px) {
+  /* @media screen and (min-width: 768px) {
     max-width: 768px;
     padding: 0 32px;
-  }
+  } */
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: 768px) {
     max-width: 1440px;
     padding: 0 96px;
   }
