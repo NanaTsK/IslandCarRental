148134d5744d78b3cfdc2933b8c8d46b1fcc0561
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2024-02-03 14:13:26 +0100

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,45 @@
+// import { configureStore } from '@reduxjs/toolkit';
+
+// import { carsReducer } from './cars/carsSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     carsReducer,
+//   },
+// });
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { configureStore } from '@reduxjs/toolkit';
 
 import { carsReducer } from './cars/carsSlice';
 
+const carsPersistConfig = {
+  key: 'cars',
+  storage,
+  whitelist: ['favorites'],
+};
+
 export const store = configureStore({
   reducer: {
-    carsReducer,
+    carsReducer: persistReducer(carsPersistConfig, carsReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
